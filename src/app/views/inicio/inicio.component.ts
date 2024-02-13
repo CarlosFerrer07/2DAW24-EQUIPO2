f88536333b2d2ca49a04b7f33b26baf8840a300e
zacky420 <albertoserrano7@hotmail.com>
2024-02-13 08:40:11 +0100

    Primer Commit rama Inicio

--- src/app/views/inicio/inicio.component.ts
+++ src/app/views/inicio/inicio.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { JumbotronComponent } from '../../components/Jumbotron/jumbotron/jumbotron.component';
+import { CardComponent } from '../../components/Card/card/card.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [JumbotronComponent,CardComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+images= ["https://pxbar.com/wp-content/uploads/2023/09/goofy-ahh-pictures-14.jpg","https://pxbar.com/wp-content/uploads/2023/09/goofy-ahh-pictures-15.jpg","https://pxbar.com/wp-content/uploads/2023/09/wallpaper-goofy-ahh-pictures.jpg"]
+Title= ["RAHHH","aASDSGFHF","CXZVBVN"]
+Text= ["","",""]
 }
