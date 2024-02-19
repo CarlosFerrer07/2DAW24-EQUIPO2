46c8470cc3c70536c37a37f1f74caff6bedfce45
Carlos Ferrer <cafema01@floridauniversitaria.es>
2024-02-19 10:05:46 +0100

    recogemos los datos con form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/views/inscripcion/inscripcion.component.ts
+++ src/app/views/inscripcion/inscripcion.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-inscripcion',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inscripcion.component.html',
   styleUrl: './inscripcion.component.css'
 })
 export class InscripcionComponent {
 
+  reactiveForm = new FormGroup({
+    nombre: new FormControl('',{ nonNullable:true}),
+    apellidos: new FormControl('',{ nonNullable:true}),
+    dni: new FormControl('',{ nonNullable:true}),
+    pasaporte: new FormControl('',{ nonNullable:true}),
+    email: new FormControl('',{ nonNullable:true}),
+    tel: new FormControl('',{ nonNullable:true}),
+    comentario: new FormControl('',{ nonNullable:true}),
+  });
+
+  onSubmit() {
+    console.log(this.reactiveForm.value);
+  }
 }
